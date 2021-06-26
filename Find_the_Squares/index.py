11e245c1acecc0cf1cbaa48ed179ca74f4d02a6c
Alex <alexavalos1990@gmail.com>
2021-06-26 16:10:17 -0400

    add code kata

---
+++ Find_the_Squares/index.py
@@ -0,0 +1,14 @@
+"""Problem
+Complete the function that takes an odd integer (0 < n < 1000000) which is the difference between two consecutive perfect squares, and return these squares as a string in the format "bigger-smaller"
+
+Examples
+9  -->  "25-16"
+5  -->  "9-4"
+7  -->  "16-9"
+"""
+
+def find_squares(num):
+    lower = int((num - 1)/2)
+    upper = lower + 1
+    return f"{upper**2}-{lower**2}"
+    
